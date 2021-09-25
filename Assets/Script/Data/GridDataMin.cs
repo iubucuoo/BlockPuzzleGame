34716b0e74b967d97640478a43941701154a5af2
wsj <522742475@qq.com>
2021-09-25 09:44:03 +0800

    add state

---
+++ Assets/Script/Data/GridDataMin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GridDataMin : GridData
+{
+    public override IPoolsType GroupType => IPoolsType.GridDataMin;
+
+    public override void OnRecycled()
+    {
+        base.OnRecycled();
+    }
+}
