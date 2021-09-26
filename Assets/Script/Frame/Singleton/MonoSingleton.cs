96341dee769a20539b007ef60c46f218666b8766
wsj <522742475@qq.com>
2021-09-26 16:56:53 +0800

    ui change
    ui调整

---
+++ Assets/Script/Frame/Singleton/MonoSingleton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace IUBCOFrame
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+    {
+        public static T instance
+        {
+            get
+            {
+                CheckInstance();
+                return m_Instance;
+            }
+        }
+
+        public static void CheckInstance()
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = FindObjectOfType<T>();
+                if (m_Instance == null)
+                {
+                    GameObject gameObject = new GameObject(typeof(T).Name);
+                    DontDestroyOnLoad(gameObject);
+                    m_Instance = gameObject.AddComponent<T>();
+                }
+            }
+        }
+
+        public static bool hasInstance
+        {
+            get
+            {
+                return m_Instance != null;
+            }
+        }
+        protected virtual void Awake()
+        {
+            if (!m_Instance)
+            {
+                m_Instance = (T)this;
+                if (transform.parent == null)
+                {
+                    DontDestroyOnLoad(this);
+                    return;
+                }
+            }
+            else if (m_Instance != this)
+            {
+                Debug.LogErrorFormat("Instance of {0} already exist, this destroyed!", new object[]
+                {
+                    name
+                });
+                Destroy(this);
+            }
+        }
+        protected static T m_Instance;
+    }
+}
---
+++ Assets/Script/Frame/Singleton/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace IUBCOFrame
+{
+    public class Singleton<T> where T : new()
+    {
+        protected Singleton()
+        {
+        }
+
+        public static T instance
+        {
+            get
+            {
+                if (s_Instance == null)
+                {
+                    object obj = s_Lock;
+                    lock (obj)
+                    {
+                        if (s_Instance == null)
+                        {
+                            s_Instance = Activator.CreateInstance<T>();
+                        }
+                    }
+                }
+                return s_Instance;
+            }
+        }
+
+
+        private static T s_Instance = default;
+
+
+        private static object s_Lock = new object();
+    }
+}
