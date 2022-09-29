b5793fa3f914ba36aff497f89bd7d13b3a44a4b9
wsj <522742475@qq.com>
2022-09-29 14:18:17 +0800

    添加默认禁用触摸 不渲染触摸

---
+++ Assets/Editor/UITools/UIRaycastTargetOverride.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEditor.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Reflection;
+
+[InitializeOnLoad]
+static internal class UIRaycastTargetOverride
+{
+    //The reflected dafault methods.
+    private static MethodInfo m_miGetDefaultResource = null;
+    private static MethodInfo m_miPlaceUIElementRoot = null;
+
+    static UIRaycastTargetOverride()
+    {
+        Initialize();
+    }
+
+    private static void Initialize()
+    {
+        //Get all loaded assemblies.
+        Assembly[] allAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+        Assembly uiEditorAssembly = null;
+        foreach (Assembly assembly in allAssemblies)
+        {
+            AssemblyName assemblyName = assembly.GetName();
+            if ("UnityEditor.UI" == assemblyName.Name)
+            {
+                uiEditorAssembly = assembly;
+                break;
+            }
+        }
+
+        //Check if we find ui assembly.
+        if (null == uiEditorAssembly)
+        {
+            Debug.LogError("Can not find assembly: UnityEditor.UI.dll");
+            return;
+        }
+
+        //Get things we need.
+        Type menuOptionType = uiEditorAssembly.GetType("UnityEditor.UI.MenuOptions");
+        m_miGetDefaultResource = menuOptionType.GetMethod("GetStandardResources", BindingFlags.NonPublic | BindingFlags.Static);
+        m_miPlaceUIElementRoot = menuOptionType.GetMethod("PlaceUIElementRoot", BindingFlags.NonPublic | BindingFlags.Static);
+    }
+
+    [MenuItem("GameObject/UI/Text", false, 2000)]
+    static public void AddText(MenuCommand menuCommand)
+    {
+        GameObject go = DefaultControls.CreateText((DefaultControls.Resources)m_miGetDefaultResource.Invoke(null, null));
+        m_miPlaceUIElementRoot.Invoke(null, new object[] { go, menuCommand });
+
+        //Remove raycast target.
+        Text text = go.GetComponent<Text>();
+        text.raycastTarget = false;
+    }
+
+    [MenuItem("GameObject/UI/Image", false, 2000)]
+    static public void AddImage(MenuCommand menuCommand)
+    {
+        GameObject go = DefaultControls.CreateImage((DefaultControls.Resources)m_miGetDefaultResource.Invoke(null, null));
+        m_miPlaceUIElementRoot.Invoke(null, new object[] { go, menuCommand });
+
+        //Remove raycast target.
+        Image image = go.GetComponent<Image>();
+        image.raycastTarget = false;
+    }
+
+    [MenuItem("GameObject/UI/Raw Image", false, 2000)]
+    static public void AddRawImage(MenuCommand menuCommand)
+    {
+        GameObject go = DefaultControls.CreateRawImage((DefaultControls.Resources)m_miGetDefaultResource.Invoke(null, null));
+        m_miPlaceUIElementRoot.Invoke(null, new object[] { go, menuCommand });
+
+        //Remove raycast target.
+        RawImage rawImage = go.GetComponent<RawImage>();
+        rawImage.raycastTarget = false;
+    }
+    [MenuItem("GameObject/UI/NoDrawGraphic", false, 2000)]
+    static public void AddNoDrawGraphic(MenuCommand menuCommand)
+    {
+        GameObject go = DefaultControls.CreateImage((DefaultControls.Resources)m_miGetDefaultResource.Invoke(null, null));
+        m_miPlaceUIElementRoot.Invoke(null, new object[] { go, menuCommand });
+
+        //不知道咋加新的 先这样写这
+        GameObject.DestroyImmediate(go.GetComponent<Image>());
+        go.AddComponent<NoDrawGraphic>();
+        go.name = "NoDrawGraphic";
+
+    }
+}
---
+++ Assets/Script/Utils/UI/Modules/NoDrawGraphic.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+//不会产生dc的透明射线目标
+public class NoDrawGraphic : Graphic
+{
+    protected override void Awake()
+    {
+        canvasRenderer.cullTransparentMesh = true;
+        color = UnityEngine.Color.clear;
+    }
+    //不加入渲染列队
+    public override void Rebuild(CanvasUpdate update)
+    {
+        return;
+    }
+
+
+#if UNITY_EDITOR
+    [SerializeField,Tooltip("是否显示Rect")]
+    public bool isShowRect = false;
+    [SerializeField, Tooltip("选中时是否显示Rect")]
+    public bool isShowRectSelected = true;
+    [SerializeField, Tooltip("显示的Rect颜色")]
+    public Color rectColor = new Color(0.8f, 0.1f, 0.1f, 0.3f);
+    private Texture2D _tex2D;
+
+    protected override void OnValidate()
+    {
+        if (_tex2D == null)
+        {
+            _tex2D = new Texture2D(1, 1);
+            _tex2D.wrapMode = TextureWrapMode.Repeat;
+        }
+        _tex2D.SetPixel(0, 0, rectColor);
+        _tex2D.Apply();
+    }
+    public void OnDrawGizmos()
+    {
+        if (!isShowRect)
+            return;
+        if (_tex2D == null)
+        {
+            _tex2D = new Texture2D(1, 1);
+            _tex2D.wrapMode = TextureWrapMode.Repeat;
+            _tex2D.SetPixel(0, 0, rectColor);
+            _tex2D.Apply();
+        }
+        Vector2 worldPos = RectTransformUtility.WorldToScreenPoint(this.canvas.worldCamera, transform.TransformPoint(new Vector3(rectTransform.rect.x, rectTransform.rect.y, 0)));
+        Rect rect = new Rect(worldPos.x, worldPos.y, rectTransform.rect.width, rectTransform.rect.height);
+        Gizmos.DrawGUITexture(rect, _tex2D);
+    }
+    public void OnDrawGizmosSelected()
+    {
+        if (isShowRect||!isShowRectSelected)
+            return;
+        if (UnityEditor.Selection.activeGameObject != this.gameObject)
+            return;
+        if (_tex2D == null)
+        {
+            _tex2D = new Texture2D(1, 1);
+            _tex2D.wrapMode = TextureWrapMode.Repeat;
+            _tex2D.SetPixel(0, 0, rectColor);
+            _tex2D.Apply();
+        }
+        Vector2 worldPos = RectTransformUtility.WorldToScreenPoint(this.canvas.worldCamera, transform.TransformPoint(new Vector3(rectTransform.rect.x, rectTransform.rect.y, 0)));
+        Rect rect = new Rect(worldPos.x, worldPos.y, rectTransform.rect.width, rectTransform.rect.height);
+        Gizmos.DrawGUITexture(rect, _tex2D);
+    }
+#endif
+}
